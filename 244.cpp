946ba520db50d0648381dad0ca23a381023ecf62
yuichi.sugiyama <mmxsrup@yahoo.co.jp>
2016-06-22 22:51:04 +0900

    n個の頂点があればn-1個の経路があれば全てつながる

---
+++ 244.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pint;
+typedef vector<int> vint;
+typedef vector<pint> vpint;
+#define mp make_pairt
+#define fi first
+#define se second
+#define all(v) (v).begin(),(v).end()
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define reps(i,f,n) for(int i=(f);i<(n);i++)
+
+int main(void){
+	int n;
+	cin >> n;
+	printf("%d\n", n - 1);
+	return 0;
+}
